6ebe8c76f26ac6fd04b74d0a6e5ac639e41fbd25
Brandi Parsons <branpar2@gmail.com>
2022-08-13 23:16:29 -0400

    adding play game function

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 let userName = window.prompt ('Please enter your name:')
 let userScore = 0
 
-    for(let i = 0; i < questions.length; i++){
+function playGame() {
+     for(let i = 0; i < questions.length; i++){
      let question = question[i]
-     let userAnswer = window.prompt(question[i].text)
+     let userAnswer = window.prompt(question.text)
      if(userAnswer === question.correctAnswer){
         userScore = userScore + 10
      }
     }
 
     windows.alert('your score is: '+userScore)
+   }
