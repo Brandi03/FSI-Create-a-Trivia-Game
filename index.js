f6e9a633aa0d765f7da64a9582196aa692c66ddc
Brandi Parsons <branpar2@gmail.com>
2022-08-13 21:57:25 -0400

    correct answers

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let userName = window.prompt ('Please enter your name:')
 
     for(let i = 0; i < questions.length; i++){
      let question = question[i]
-     let userAnswer = window.prompt(question.text)
+     let userAnswer = window.prompt(question[i].text)
+     if(userAnswer === question.correctAnswer){
+     }
     }
-
