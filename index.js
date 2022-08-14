62545f15c09ca612334e715413b5b2e4fb9a3a30
Brandi Parsons <branpar2@gmail.com>
2022-08-13 21:33:08 -0400

    adding questions

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 let userName = window.prompt ('Please enter your name:')
+
+    for(let i = 0; i < questions.length; i++){
+     let question = question[i]
+     let userAnswer = window.prompt(question.text)
+    }
+
