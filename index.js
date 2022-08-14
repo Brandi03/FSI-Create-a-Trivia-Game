3bbc8f2237f363ae7f44c7cf0c70578dd62087e9
Brandi Parsons <branpar2@gmail.com>
2022-08-13 22:10:04 -0400

    adding score

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 let userName = window.prompt ('Please enter your name:')
+let userScore = 0
 
     for(let i = 0; i < questions.length; i++){
      let question = question[i]
      let userAnswer = window.prompt(question[i].text)
      if(userAnswer === question.correctAnswer){
+        userScore = userScore + 10
      }
     }
+
+    windows.alert('your score is: '+userScore)
